090af452c383d3c3c204fa027930b105c467fbb8
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-21 22:03:35 +0100

    started on design for user layout

--- src/components/layout/UserLayout.tsx
+++ src/components/layout/UserLayout.tsx
@@ -1,8 +1,20 @@
 import { AppLoader } from "components/appLoader/AppLoader";
+import { Header } from "components/header/Header";
+import { Sidebar } from "components/sidebar/Sidebar";
 
 type Props = {
   children: React.ReactNode;
 };
 export const UserLayout = ({ children }: Props) => {
-  return <AppLoader>{children}</AppLoader>;
+  return (
+    <AppLoader>
+      <div className="flex flex-row">
+        <Sidebar />
+        <div className="flex flex-col w-full">
+          <Header />
+          {children}
+        </div>
+      </div>
+    </AppLoader>
+  );
 };
