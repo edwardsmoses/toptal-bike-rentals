6310e54d35705ed8d4bae90d0b5fe4f649af4147
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-21 21:29:35 +0100

    Completed Design for Sign in and register

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -2,10 +2,15 @@ import { AuthLayout } from "components/layout/AuthLayout";
 import { Label, TextInput, Checkbox, Button } from "flowbite-react";
 import Link from "next/link";
 
-const Login = () => {
+const Register = () => {
   return (
-    <AuthLayout title="Sign In into BikeRentals">
+    <AuthLayout title="Welcome to BikeRentals">
       <form action="#" className="grid grid-cols-6 gap-6 mt-8">
+        <div className="col-span-6">
+          <Label htmlFor="fullName" value="Full Name" />
+          <TextInput id="fullName" type="name" placeholder="Edwards" required={true} />
+        </div>
+
         <div className="col-span-6">
           <Label htmlFor="email" value="Email" />
           <TextInput id="email" type="email" placeholder="edwards@bikerental.com" required={true} />
@@ -16,13 +21,18 @@ const Login = () => {
           <TextInput id="password" type="password" placeholder="" required={true} />
         </div>
 
+        <div className="col-span-6 space-x-2">
+          <Checkbox id="agree" required={true} />
+          <Label htmlFor="agree">By creating an account, you agree to our terms and conditions.</Label>
+        </div>
+
         <div className="col-span-6 space-y-2">
-          <Button type="submit">Sign in</Button>
+          <Button type="submit">Create an account</Button>
 
           <p className="mt-4 space-x-1 text-sm text-gray-500 sm:mt-0">
-            <span>Don't have an account?</span>
-            <Link href="/register">
-              <a className="text-gray-700 underline">Sign in</a>
+            <span>Already have an account?</span>
+            <Link href="/login">
+              <a className="text-gray-700 underline">Log in</a>
             </Link>
             .
           </p>
@@ -32,4 +42,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
