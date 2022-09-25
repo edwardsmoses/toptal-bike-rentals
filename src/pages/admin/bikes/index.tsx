609889740b3bf2cc98e55cb8bc31d985fab28628
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-25 09:19:50 +0100

    add manage dropdown to the bikes card on admin

--- src/pages/admin/bikes/index.tsx
+++ src/pages/admin/bikes/index.tsx
@@ -1,7 +1,8 @@
 import { EmptyState } from "components/empty/EmptyState";
 import { UserLayout } from "components/layout/UserLayout";
-import { Card, Rating } from "flowbite-react";
+import { Button, Card, Dropdown, Rating } from "flowbite-react";
 import { isEmpty, map, range } from "lodash";
+import Link from "next/link";
 import { useAppSelector } from "store/store";
 
 const Bikes = () => {
@@ -10,7 +11,12 @@ const Bikes = () => {
   return (
     <UserLayout>
       <div className="p-10">
-        <h3 className="font-sans text-2xl font-medium">My Bikes</h3>
+        <div className="flex justify-between mb-2">
+          <h3 className="font-sans text-2xl font-medium">All Bikes</h3>
+          <Link href="/admin/bikes/new">
+            <Button>New Bike</Button>
+          </Link>
+        </div>
 
         {isEmpty(allBikes) && (
           <EmptyState
@@ -26,21 +32,31 @@ const Bikes = () => {
             return (
               <div className="max-w-sm" key={bike.id}>
                 <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-                  <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {bike.model}, {bike.color}
-                  </h5>
-                  <span className="font-normal text-gray-700 dark:text-gray-400">
-                    {bike.location}
-                    <Rating>
-                      {map(range(1, bike.rating + 1), (star) => {
-                        return (
-                          <button key={star} type="button">
-                            <Rating.Star />
-                          </button>
-                        );
-                      })}
-                    </Rating>
-                  </span>
+                  <div className="relative">
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {bike.model}, {bike.color}
+                    </h5>
+                    <span className="font-normal text-gray-700 dark:text-gray-400">
+                      {bike.location}
+                      <Rating>
+                        {map(range(1, bike.rating + 1), (star) => {
+                          return (
+                            <button key={star} type="button">
+                              <Rating.Star />
+                            </button>
+                          );
+                        })}
+                      </Rating>
+                    </span>
+
+                    <div className="absolute top-1 right-1">
+                      <Dropdown label="Manage" inline={true}>
+                        <Dropdown.Item>Edit Bike</Dropdown.Item>
+                        <Dropdown.Item>View Reservations</Dropdown.Item>
+                        <Dropdown.Item>Delete Bike</Dropdown.Item>
+                      </Dropdown>
+                    </div>
+                  </div>
                 </Card>
               </div>
             );
