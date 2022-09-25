7a8f0707b8372443d8215b1604628dc4bb049375
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-25 10:44:41 +0100

    link reservation on card to reservations page

--- src/pages/admin/bikes/index.tsx
+++ src/pages/admin/bikes/index.tsx
@@ -37,9 +37,11 @@ const BikeCard = ({ bike }: BikeCardProp) => {
               <Rating.Star />
               <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">{bike.rating || 0}</p>
               <span className="mx-1.5 h-1 w-1 rounded-full bg-gray-500 dark:bg-gray-400" />
-              <span className="text-sm font-medium text-gray-900 underline dark:text-white">
-                {size(reservations)} reservations
-              </span>
+              <Link href={`/admin/bikes/reservations?id=${bike.id}`}>
+                <a className="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white">
+                  {size(reservations)} reservations
+                </a>
+              </Link>
             </Rating>
           </span>
 
