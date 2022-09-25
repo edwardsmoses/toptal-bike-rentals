35b9c3e953eb36afaa2b8fcdce0047833fec400a
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-25 09:14:25 +0100

    Updated the Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,13 @@
+import { auth } from "firebase-app/init";
+import { Dropdown } from "flowbite-react";
+import { first, words } from "lodash";
+import { currentUserActions } from "store/features/currentUserSlice";
+import { useAppDispatch, useAppSelector } from "store/store";
+
 export const Header = () => {
+  const dispatch = useAppDispatch();
+  const currentUser = useAppSelector((state) => state.currentUser.user);
+
   return (
     <header className="w-full bg-gray-50">
       <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8">
@@ -13,7 +22,7 @@ export const Header = () => {
               className="w-full h-10 pl-4 pr-10 text-sm bg-white border-none rounded-lg shadow-sm sm:w-56"
               id="search"
               type="search"
-              placeholder="Search website..."
+              placeholder="Search bikes..."
             />
 
             <button
@@ -35,105 +44,42 @@ export const Header = () => {
           </div>
 
           <div className="flex items-center justify-between flex-1 gap-8 sm:justify-end">
-            <div className="flex gap-4">
-              <button
-                type="button"
-                className="block sm:hidden p-2.5 text-gray-600 bg-white rounded-lg hover:text-gray-700 shrink-0 shadow-sm"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            <Dropdown
+              inline={true}
+              placement="bottom-end"
+              label={
+                <div className="flex items-center transition rounded-lg group shrink-0">
+                  <img
+                    alt="Man"
+                    src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                    className="object-cover w-10 h-10 rounded-full"
                   />
-                </svg>
-              </button>
 
-              <a
-                href="#"
-                className="block p-2.5 text-gray-600 bg-white rounded-lg hover:text-gray-700 shrink-0 shadow-sm"
+                  <p className="hidden ml-2 text-xs text-left sm:block">
+                    <strong className="block font-medium">{currentUser.fullName}</strong>
+                    <span className="text-gray-500"> {currentUser.email} </span>
+                  </p>
+                </div>
+              }
+            >
+              <Dropdown.Item
+                onClick={() => {
+                  auth.signOut();
+                  dispatch(currentUserActions.resetCurrentUser());
+                }}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  />
-                </svg>
-              </a>
-
-              <a
-                href="#"
-                className="block p-2.5 text-gray-600 bg-white rounded-lg hover:text-gray-700 shrink-0 shadow-sm"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-5 h-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
-              </a>
-            </div>
-
-            <button type="button" className="flex items-center transition rounded-lg group shrink-0">
-              <img
-                alt="Man"
-                src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                className="object-cover w-10 h-10 rounded-full"
-              />
-
-              <p className="hidden ml-2 text-xs text-left sm:block">
-                <strong className="block font-medium">Eric Frusciante</strong>
-
-                <span className="text-gray-500"> eric@frusciante.com </span>
-              </p>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="hidden w-5 h-5 ml-4 text-gray-500 transition sm:block group-hover:text-gray-700"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </button>
+                Sign Out
+              </Dropdown.Item>
+            </Dropdown>
           </div>
         </div>
 
         <div className="mt-8">
-          <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">Welcome Back, Barry!</h1>
+          <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+            Welcome Back, {first(words(currentUser.fullName))}!
+          </h1>
 
-          <p className="mt-1.5 text-sm text-gray-500">
-            Your website has seen a 52% increase in traffic in the last month. Keep it up! 🚀
-          </p>
+          <p className="mt-1.5 text-sm text-gray-500">Get on the road - Make a reservation today 🚀</p>
         </div>
       </div>
     </header>
