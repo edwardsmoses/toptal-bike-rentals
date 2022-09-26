f8292e1a86e38c99c4a7a699d75ca5ab3d93f957
Edwards Moses <edwardsmoses3@gmail.com>
2022-09-26 18:29:06 +0100

    feat:home: Update the HomePage background image. Update the button copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
-    <section className="relative bg-center bg-no-repeat bg-cover bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)]">
+    <section className="relative bg-center bg-no-repeat bg-cover bg-[url(https://images.unsplash.com/photo-1585822797375-2a38044b8499?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80)]">
       <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"></div>
 
       <div className="relative max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:px-8 lg:h-screen lg:items-center lg:flex">
@@ -16,13 +16,13 @@ const Home: NextPage = () => {
           <div className="flex flex-wrap gap-4 mt-8 text-center">
             <Link href="/login">
               <a className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-rose-600 sm:w-auto active:bg-rose-500 hover:bg-rose-700 focus:outline-none focus:ring">
-                Get Started
+                Sign in to your Account
               </a>
             </Link>
 
-            <Link href="/">
+            <Link href="/register">
               <a className="block w-full px-12 py-3 text-sm font-medium bg-white rounded shadow text-rose-600 sm:w-auto hover:text-rose-700 active:text-rose-500 focus:outline-none focus:ring">
-                Learn More
+                Get Started - Create an Account
               </a>
             </Link>
           </div>
